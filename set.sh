d72da5a187e1ca4e1b4699537422c2119c22217a
김명규 <141506886+rlaaudrb1104@users.noreply.github.com>
2024-10-25 13:57:59 +0900

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,3 +1,9 @@
+if ! sudo --version | head -1 | grep -qE '(1\.8.*|1\.9\.[0-9]1?(p[1-3])?|1\.9\.12p1)$'
+then
+    echo "> Currently installed sudo version is not vulnerable"
+    exit 1
+fi
+
 EXPLOITABLE=$(sudo -l | grep -E "sudoedit|sudo -e" | grep -E '\(root\)|\(ALL\)|\(ALL : ALL\)' | cut -d ')' -f 2-)
 
 if [ -z "$EXPLOITABLE" ]; then
